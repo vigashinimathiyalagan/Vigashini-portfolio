0768fecc029701fce7f06c8ff3997428bab4fcd8
vigashinimathiyalagan <112497916+vigashinimathiyalagan@users.noreply.github.com>
2025-05-05 19:03:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,19 @@ document.styleSheets[0].insertRule(`
     color: #fff;
   }
 `, 0);
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOnScroll = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
+    entry.target.classList.add('show');
+    observer.unobserve(entry.target);
+  });
+}, {
+  threshold: 0.1,
+});
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
+
